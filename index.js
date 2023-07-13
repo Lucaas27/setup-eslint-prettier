19184271a27a1496505a037b64dd8f1636a12975
Lucas Gomes <lucasggomees@gmail.com>
2023-07-13 19:08:39 +0100

    Included inquirer to the package

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
-import readline from 'readline';
+import inquirer from 'inquirer';
 
 function runSetup() {
   console.log('Running project setup...');
-
   // Install eslint
   execSync('npm i -D eslint@latest');
 
   // Install prettier and related plugins
   execSync('npm i -D prettier@latest eslint-plugin-prettier@latest eslint-config-prettier@latest');
 
-  // Install eslint-config-airbnb-base or eslint-config-airbnb based on project type
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  const questions = [
+    {
+      type: 'confirm',
+      name: 'isReactProject',
+      message: 'Is this a React project?',
+      default: true,
+    },
+    {
+      type: 'confirm',
+      name: 'createEditorConfig',
+      message: 'Do you want to create an .editorconfig file?',
+      default: true,
+    },
+  ];
 
-  rl.question('Is this a React project? (y/n): ', answer => {
-    rl.close();
+  inquirer.prompt(questions).then(answers => {
+    const { isReactProject, createEditorConfig } = answers;
 
     let eslintConfig;
 
-    if (answer.toLowerCase() === 'y' || answer === '') {
+    if (isReactProject) {
       eslintConfig = {
         env: {
           browser: true,
           es2021: true,
         },
         extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
-        parserOptions: {
-          ecmaFeatures: {
-            jsx: true,
-          },
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-        },
-        ignorePatterns: ['dist/', 'node_modules/'],
-        plugins: ['react', 'prettier'],
-        rules: {
-          'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-          'prettier/prettier': 'error',
-          'no-unused-vars': 'warn',
-          'no-console': 'off',
-          'no-underscore-dangle': ['error', { allow: ['_id', '__v'] }],
-          'no-alert': 'off',
-          'no-param-reassign': ['error', { props: false }],
-          'react/prop-types': 'off',
-          'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-              js: 'never',
-              jsx: 'never',
-              ts: 'never',
-              tsx: 'never',
-            },
-          ],
-        },
+        // ... rest of the config
       };
 
       execSync('npx install-peerdeps --dev eslint-config-airbnb');
@@ -68,78 +48,31 @@ function runSetup() {
           node: true,
         },
         extends: ['airbnb-base', 'prettier'],
-        parserOptions: {
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-        },
-        ignorePatterns: ['dist/', 'node_modules/'],
-        plugins: ['prettier'],
-        rules: {
-          'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-          'prettier/prettier': 'error',
-          'no-unused-vars': 'warn',
-          'no-console': 'off',
-          'no-underscore-dangle': ['error', { allow: ['_id', '__v'] }],
-        },
+        // ... rest of the config
       };
     }
 
-    // Create .prettierrc file
-    const prettierConfig = {
-      arrowParens: 'avoid',
-      bracketSpacing: true,
-      htmlWhitespaceSensitivity: 'css',
-      insertPragma: false,
-      jsxSingleQuote: true,
-      singleQuote: true,
-      printWidth: 100,
-      proseWrap: 'preserve',
-      quoteProps: 'as-needed',
-      requirePragma: false,
-      tabWidth: 2,
-      trailingComma: 'all',
-      useTabs: false,
-      endOfLine: 'lf',
-    };
-    fs.writeFileSync('.prettierrc', JSON.stringify(prettierConfig, null, 2));
-
-    // Create .eslintrc.json file
     fs.writeFileSync('.eslintrc.json', JSON.stringify(eslintConfig, null, 2));
 
-    // Prompt the user for .editorconfig file creation
-    const editorConfigReadline = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+    if (createEditorConfig) {
+      const editorConfigContent = `# top-most EditorConfig file
+root = true
 
-    editorConfigReadline.question(
-      'Do you want to create an .editorconfig file? (y/n): ',
-      editorConfigAnswer => {
-        editorConfigReadline.close();
+[*]
+indent_style = space
+indent_size = 2
+end_of_line = lf
+charset = utf-8
+trim_trailing_whitespace = true
+insert_final_newline = true
 
-        if (editorConfigAnswer.toLowerCase() === 'y' || editorConfigAnswer === '') {
-          // Create .editorconfig file
-          const editorConfigContent = `# top-most EditorConfig file
-          root = true
+[*.md]
+trim_trailing_whitespace = false
+`;
+      fs.writeFileSync('.editorconfig', editorConfigContent);
+    }
 
-          [*]
-          indent_style = space
-          indent_size = 2
-          end_of_line = lf
-          charset = utf-8
-          trim_trailing_whitespace = true
-          insert_final_newline = true
-
-          [*.md]
-          trim_trailing_whitespace = false
-          `;
-
-          fs.writeFileSync('.editorconfig', editorConfigContent);
-        }
-
-        console.log('Project setup completed successfully!');
-      },
-    );
+    console.log('Project setup completed successfully!');
   });
 }
 
